b42d7e764d5b6cfad9897b0d48241b26ebb19eb2
jhoanjg <jhoan@dataico.com>
2025-02-20 15:28:11 -0500

    - Letter 'L' shape - Control color for each shape

--- src/main/java/org/example/Block.java
+++ src/main/java/org/example/Block.java
@@ -5,9 +5,11 @@ import org.example.lib.Vector;
 
 public abstract class Block {
     Vector position ;
+    private final TextColor.ANSI foregroundColor;
 
-    public Block(Vector position) {
+    public Block(Vector position, TextColor.ANSI color) {
         this.position = position;
+        this.foregroundColor = color;
     }
 
     public abstract void rotate();
@@ -23,11 +25,11 @@ public abstract class Block {
 
     public void draw(TerminalDisplay t) {
         Vector[] shape = getSquares();
+        t.setForegroundColor(foregroundColor);
 
         for(Vector v : shape) {
             Vector blockPosition = position.plus(v);
             t.putCharacter(blockPosition.getRoundedX(), blockPosition.getRoundedY(), '*');
-            t.setForegroundColor(TextColor.ANSI.RED);
         }
     }
 
---
+++ src/main/java/org/example/LShape.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import com.googlecode.lanterna.TextColor;
+import org.example.lib.Vector;
+
+public class LShape extends Block {
+    public int rotation = 0;
+    private final Vector[][] rotations = new Vector[4][4];
+
+    public LShape(Vector initialPosition) {
+        super(initialPosition, TextColor.ANSI.RED);
+        rotations[0][0] = new Vector(0, 0);
+        rotations[0][1] = new Vector(0, 1);
+        rotations[0][2] = new Vector(0, 2);
+        rotations[0][3] = new Vector(1, 2);
+
+        rotations[1][0] = new Vector(0, 0);
+        rotations[1][1] = new Vector(1, 0);
+        rotations[1][2] = new Vector(2, 0);
+        rotations[1][3] = new Vector(0, 1);
+
+        rotations[2][0] = new Vector(0, 0);
+        rotations[2][1] = new Vector(1, 0);
+        rotations[2][2] = new Vector(1, 1);
+        rotations[2][3] = new Vector(1, 2);
+
+        rotations[3][0] = new Vector(0, 0);
+        rotations[3][1] = new Vector(1, 0);
+        rotations[3][2] = new Vector(2, 0);
+        rotations[3][3] = new Vector(2, -1);
+    }
+
+    @Override
+    public void rotate() {
+        rotation = (rotation + 1) % 4;
+    }
+
+    @Override
+    public Vector[] getSquares() {
+        return rotations[rotation];
+    }
+}
