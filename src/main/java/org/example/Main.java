384879459de113af4361e528e154a08a4b4f3c16
dataicomacbook8 <davidjimenezch20@gmail.com>
2025-02-20 09:15:59 -0500

    Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,73 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import com.googlecode.lanterna.TerminalSize;
+import org.example.lib.Vector;
+import com.googlecode.lanterna.input.KeyStroke;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        TerminalDisplay t = new TerminalDisplay();
+        Ball b = new Ball(new Vector(3, 5), new Vector(30, 6));
+        TerminalSize size = t.getTerminalSize();
+        int height = size.getRows();
+        int width = size.getColumns();
+        Paddle playerOne = new Paddle(new Vector(2, height / 2.0), 5);
+        Paddle playerTwo = new Paddle(new Vector(width - 2, height / 2.0), 5);
+
+        double fps = 30.0;
+        double timeDelta = 1.0 / fps;
+        final long timeDeltaMs = (long) (timeDelta * 1000);
+        System.out.println(timeDeltaMs);
+        KeyStroke k;
+
+        do {
+            long start = System.currentTimeMillis();
+
+            b.updatePosition(timeDelta);
+            t.clear();
+            playerOne.draw(t);
+            playerTwo.draw(t);
+
+            if (b.position.x >= width || b.position.x <= 0) {
+                System.out.println("You lose!! :(");
+                System.exit(1);
+            }
+
+            if (b.position.y >= height || b.position.y <= 0)
+                b.bounce(Axis.Y);
+
+            if (playerOne.isBallCollidingPaddle(b) || playerTwo.isBallCollidingPaddle(b)) {
+                b.bounce(Axis.X);
+            }
+
+            k = t.getNextKeypress();
+            if (k != null)
+                switch (k.getCharacter()) {
+                    case 'w':
+                        playerOne.moveUp();
+                        break;
+                    case 's':
+                        playerOne.moveDown(t);
+                        break;
+                    case 'k':
+                        playerTwo.moveUp();
+                        break;
+                    case 'j':
+                        playerTwo.moveDown(t);
+                        break;
+                }
+
+            t.setCursorPosition((int) Math.round(b.position.x), (int) Math.round(b.position.y));
+
+            t.flush();
+            long end = System.currentTimeMillis();
+            long timeToRender = end - start;
+
+            Thread.sleep(Math.max(0, timeDeltaMs - timeToRender));
+
+
         }
+        while (k == null || k.getCharacter() != 'q');
+        System.exit(1);
     }
 }
