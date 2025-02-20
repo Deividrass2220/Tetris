226678d51699bf011907e2bde821d9a7972df9f1
Shakira Galvan <shakira@dataico.com>
2025-02-20 17:31:36 -0500

    Field draw implementation

--- src/main/java/org/example/Field.java
+++ src/main/java/org/example/Field.java
@@ -6,12 +6,37 @@ import org.example.lib.Vector;
 import java.util.Arrays;
 
 public class Field {
+    public final int width, height;
     public TextColor.ANSI[][] field;
 
     public Field(int width, int height) {
+        this.width = width;
+        this.height = height;
         this.field = new TextColor.ANSI[width][height];
         for (TextColor.ANSI[] row : field)
             Arrays.fill(row, TextColor.ANSI.BLACK);
+    }
+
+    public void draw(TerminalDisplay t) {
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++)
+                if (field[x][y] != TextColor.ANSI.BLACK) {
+                    t.setForegroundColor(field[x][y]);
+                    t.putCharacter(x, y, '*');
+                }
+    }
+
+    public boolean hasLanded(Block b) {
+        Vector[] relativePositions = b.getRelativePositionsOfSquares();
+        Vector position = b.getPosition();
+
+        for (Vector relativePosition : relativePositions) {
+            Vector absPosition = position.plus(relativePosition);
+            if (field[absPosition.getRoundedX()][absPosition.getRoundedY() + 1] != TextColor.ANSI.BLACK)
+                return true;
+        }
+
+        return false;
     }
 
     public void fillBlock(Block b) {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,19 +5,33 @@ import org.example.lib.Vector;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         TerminalDisplay t = new TerminalDisplay();
+        Field field = new Field(10, 20);
+        Block square = new Square(new Vector(0, 18));
+        Block lShape = new LShape(new Vector(3, 16));
+        Block tShape = new TShape(new Vector(7, 16));
+        Block line = new Line(new Vector(9, 16));
 
-        Block square = new Square(new Vector(3, 4));
-        Block lShape = new LShape(new Vector(6, 2));
-        Block tShape = new TShape(new Vector(7, 8));
-        Block line = new Line(new Vector(10, 5));
+        field.fillBlock(square);
+        field.fillBlock(lShape);
+        field.fillBlock(tShape);
+        field.fillBlock(line);
 
-        square.draw(t);
-        lShape.draw(t);
-        tShape.draw(t);
-        line.draw(t);
+        Block shape = new Square(new Vector(5, 0));
 
-
-
+        for (int y = 0; y < 18; y++) {
+            t.clear();
+            field.draw(t);
+            shape.position.y = y;
+            if (!field.hasLanded(shape))
+                shape.draw(t);
+            else {
+                field.fillBlock(shape);
+                y = 0;
+                shape = new Line(new Vector(5, 2));
+            }
+            t.flush();
+            Thread.sleep(1000);
+        }
     }
 
 }
