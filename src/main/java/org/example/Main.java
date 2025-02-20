6ff9846d4e2f493b7bad8108eca5aad665fab1f4
jhoanjg <jhoan@dataico.com>
2025-02-20 12:53:54 -0500

    Cleaning Main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,8 @@
 package org.example;
 
-import org.example.lib.Vector;
-
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         TerminalDisplay t = new TerminalDisplay();
-        VerticalLine vLine = new VerticalLine(5, new Vector(5, 5), new Vector(3, 3));
-        HorizontalLine hLine = new HorizontalLine(5, new Vector(10, 10), new Vector(8, 3));
-        vLine.draw(t);
-        hLine.draw(t);
 
     }
 
